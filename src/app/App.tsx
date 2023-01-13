2390dfd07c26f554b756a0adc14fe3891849a497
Kristina <kira7999@gmail.com>
2023-01-13 17:42:35 +0300

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/jsx-wrap-multilines */
+import { Route, Routes } from 'react-router-dom';
+
+function App(): JSX.Element {
+  return (
+    <div className="layout">
+      <div>Header</div>
+      <div className="layout_page">
+        <main>
+          <Routes>
+            <Route path="/" element={<div>Main Page</div>} />
+            <Route path="/sign-up" element={<div>SignUp Page</div>} />
+            <Route path="/sign-in" element={<div>SignIn Page</div>} />
+            <Route path="/admin" element={<div>Admin Page</div>} />
+            <Route path="/client" element={<div>Client Page</div>} />
+            <Route path="/manager" element={<div>Manager Page</div>} />
+            <Route path="/doctor" element={<div>Doctor Page</div>} />
+            <Route path="/forum" element={<div>Forum Page</div>} />
+            <Route path="/petFindPage" element={<div>PetFind Page</div>} />
+          </Routes>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
