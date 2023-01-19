d3e2db44d237ba131fa7e0caed4ec472c586ea1c
wolfgray <5565486@mail.ru>
2023-01-20 01:54:10 +0300

    add landing page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,16 @@
 /* eslint-disable react/jsx-wrap-multilines */
 import { Route, Routes } from 'react-router-dom';
+import Landing from '../view/Landing';
 
 function App(): JSX.Element {
   return (
     <div className="layout">
       <div>Header</div>
       <div className="layout_page">
+        {/* <Landing /> */}
         <main>
           <Routes>
-            <Route path="/" element={<div>Main Page</div>} />
+            <Route path="/" element={<Landing />} />
             <Route path="/sign-up" element={<div>SignUp Page</div>} />
             <Route path="/sign-in" element={<div>SignIn Page</div>} />
             <Route path="/admin" element={<div>Admin Page</div>} />
---
+++ src/view/LandingObjects.tsx
@@ -0,0 +1,104 @@
+import p126 from '../assets/Landing/icons/Rectangle126.png';
+import p127 from '../assets/Landing/icons/Rectangle127.png';
+import p128 from '../assets/Landing/icons/Rectangle128.png';
+import p129 from '../assets/Landing/icons/Rectangle129.png';
+import p130 from '../assets/Landing/icons/Rectangle130.png';
+
+import cat from '../assets/Landing/icons/Rectangle119.png';
+import dog from '../assets/Landing/icons/Rectangle120.png';
+import bird from '../assets/Landing/icons/Rectangle121.png';
+import griz from '../assets/Landing/icons/Rectangle122.png';
+import rept from '../assets/Landing/icons/Rectangle123.png';
+import rabbit from '../assets/Landing/icons/Rectangle124.png';
+import hor from '../assets/Landing/icons/Rectangle125.png';
+
+export const animalClass = [
+  {
+    num: '01',
+    icon: cat,
+    title: 'Кошки',
+  },
+  {
+    num: '02',
+    icon: dog,
+    title: 'Собаки',
+  },
+  {
+    num: '03',
+    icon: bird,
+    title: 'Птицы',
+  },
+  {
+    num: '04',
+    icon: griz,
+    title: 'Грызуны',
+  },
+  {
+    num: '05',
+    icon: rept,
+    title: 'Рептилии',
+  },
+  {
+    num: '06',
+    icon: rabbit,
+    title: 'Кролики',
+  },
+  {
+    num: '07',
+    icon: hor,
+    title: 'Хорьки',
+  },
+];
+
+export const tabs = [
+  {
+    icon: p126,
+    title: 'Терапия',
+    content: [
+      '- Ваш питомец заболел или его поведение вызывает у Вас тревогу',
+      '- Нужна консультация по содержанию и кормлению',
+      '- Подошла пора вакцинации',
+    ],
+    image: '../assets/Landing/Ellipse49.png',
+  },
+  {
+    icon: p127,
+
+    title: 'Вакцинация',
+    content: [
+      '- Ваш питомец заболел или его поведение вызывает у Вас тревогу',
+      '- Нужна консультация по содержанию и кормлению',
+      '- Подошла пора вакцинации',
+    ],
+  },
+  {
+    icon: p128,
+
+    title: 'Стоматология',
+    content: [
+      '- Ваш питомец заболел или его поведение вызывает у Вас тревогу',
+      '- Нужна консультация по содержанию и кормлению',
+      '- Подошла пора вакцинации',
+    ],
+  },
+  {
+    icon: p129,
+
+    title: 'Дерматология',
+    content: [
+      '- Ваш питомец заболел или его поведение вызывает у Вас тревогу',
+      '- Нужна консультация по содержанию и кормлению',
+      '- Подошла пора вакцинации',
+    ],
+  },
+  {
+    icon: p130,
+
+    title: 'Диагностика',
+    content: [
+      '- Ваш питомец заболел или его поведение вызывает у Вас тревогу',
+      '- Нужна консультация по содержанию и кормлению',
+      '- Подошла пора вакцинации',
+    ],
+  },
+];
