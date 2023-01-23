41e4d1684b25bcb8800d40756a011c3f3914a90b
Stanislav Dokshin <d08121988sv@mail.ru>
2023-01-23 10:33:48 +0300

    deleting addresses link

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -17,8 +17,8 @@ function Header(): JSX.Element {
   ) : (
     <div className={classes.accountInfo}>
       <Link className={classes.link_mobile} to=''>Аккаунт</Link>
-      <Link className={classes.link_signIn} to='/sign-in'>Sign In</Link>
-      <Link className={classes.link_register} to='/sign-up'>Register</Link>
+      <Link className={classes.link_signIn} to=''>Sign In</Link>
+      <Link className={classes.link_register} to=''>Register</Link>
     </div>
   );
 
@@ -27,14 +27,14 @@ function Header(): JSX.Element {
       <Link className={classes.menu_mobile} to='' />
       <div className={classes.imgHeader} />
       <ul className={classes.listLink}>
-        <li className={classes.link}><NavLink className={classes.navlink} to='/doctor'>Список докторов</NavLink></li>
+        <li className={classes.link}><NavLink className={classes.navlink} to=''>Список докторов</NavLink></li>
         <li className={classes.link}><NavLink className={classes.navlink} to=''>Список процедур</NavLink></li>
         <li className={classes.link}><NavLink className={classes.navlink} to=''>Список того</NavLink></li>
         <li className={classes.link}><NavLink className={classes.navlink} to=''>О чем-нубудь</NavLink></li>
         <li className={classes.link}><NavLink className={classes.navlink} to=''>Список сосисок</NavLink></li>
         <li className={classes.link}><NavLink className={classes.navlink} to=''>Контакты</NavLink></li>
         <li className={classes.link}><NavLink className={classes.navlink} to=''>О нас</NavLink></li>
-        <li className={classes.link}><NavLink className={classes.navlink} to='/forum'>Форум</NavLink></li>
+        <li className={classes.link}><NavLink className={classes.navlink} to=''>Форум</NavLink></li>
       </ul>
       {item}
     </header>
