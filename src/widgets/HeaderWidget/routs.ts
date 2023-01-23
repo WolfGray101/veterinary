8065bc2d170d2a7207bc07f4cce83c64d43c99a5
Stanislav Dokshin <d08121988sv@mail.ru>
2023-01-23 17:37:51 +0300

    refactor link list and font

---
+++ src/widgets/HeaderWidget/routs.ts
@@ -0,0 +1,12 @@
+type RoutsHeaderArray = { nameLink: string, linkTo: string, };
+
+export const routsHeaderArray: Array<RoutsHeaderArray> = [
+  { nameLink: 'Список докторов', linkTo: '', },
+  { nameLink: 'Список процедур', linkTo: '', },
+  { nameLink: 'Список того', linkTo: '', },
+  { nameLink: 'О чем нибудь', linkTo: '', },
+  { nameLink: 'Список сосисок', linkTo: '', },
+  { nameLink: 'Контакты', linkTo: '', },
+  { nameLink: 'О нас', linkTo: '', },
+  { nameLink: 'Форум', linkTo: '', },
+];
