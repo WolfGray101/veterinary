ab327e460a25e7c1938debec690b357b4310ed42
Andrey Khyshev <andrey6lock@gmail.com>
2023-01-28 11:40:10 +0300

    SP-13: Fix types ClientDTO.ts

--- src/types/ClientDTO/ClientDTO.ts
+++ src/types/ClientDTO/ClientDTO.ts
@@ -2,16 +2,16 @@ import { PetType } from '../PetsDTO/PetsDTO';
 
 interface Pet {
   'id': number,
-  'name': 'string',
-  'avatar': 'string',
+  'name': string,
+  'avatar': string,
   'birthDay': string,
   'petType': PetType,
 }
 
 export interface IClient {
-  'firstname': 'string',
-  'lastname': 'string',
-  'avatar': 'string',
-  'email': 'string',
+  'firstname': string,
+  'lastname': string,
+  'avatar': string,
+  'email': string,
   'pets': Pet[]
 }
