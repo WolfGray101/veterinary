2681ae3a14fdd5d0e8d4a494870a5cba025c1155
Andrey Khyshev <andrey6lock@gmail.com>
2023-01-29 22:16:07 +0300

    SP-19: Add styles and animations

--- src/widgets/TopicWidget/TopicWidget.tsx
+++ src/widgets/TopicWidget/TopicWidget.tsx
@@ -1,20 +1,13 @@
+import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { format } from 'date-fns';
+import { ru } from 'date-fns/locale';
+
 import classes from './TopicWidget.module.scss';
 
-// interface CommentDtoList {
-//   id: number,
-//   content: string,
-//   dateTime: Date,
-//   likes: number,
-//   dislike: number,
-// }
-//
-// interface ITopicStarter {
-//   id: number,
-//   email: string,
-//   firstname: string,
-//   lastname: string,
-// }
-//
+import calendarIcon from '../../assets/TopicWidget/icons/calendar-icon.svg';
+import commentsIcon from '../../assets/TopicWidget/icons/comments-icon.svg';
+
 interface ITopicWidgetProps {
   title: string,
   content: string,
@@ -24,23 +17,63 @@ interface ITopicWidgetProps {
   commentsCount: number,
 }
 
-const TopicWidget = ({ title, content, creationDate, lastUpdateDate, name, commentsCount }:ITopicWidgetProps): JSX.Element => (
-  <article className={classes.article}>
-    <div className={classes.articleContentContainer}>
-      <h2 className={classes.articleTitle}>{title}</h2>
-      <p className={classes.articleContent}>{content}</p>
-    </div>
-    <div className={classes.articleUserInfoContainer}>
-      <img className={classes.articleUserAvatar} src='#' alt='User avatar' />
-      <span>{name}</span>
-    </div>
-    <div className={classes.articleDateContainer}>
-      <span>{creationDate}</span>
-      <span>{lastUpdateDate}</span>
-    </div>
-    <div className={classes.articleCommentsContainer}>
-      <img src='#' alt='Comments icon' />
-      <span>{commentsCount}</span>
-    </div>
-  </article>
-);
+const TopicWidget: React.FC<ITopicWidgetProps> = ({
+  title,
+  content,
+  creationDate,
+  lastUpdateDate,
+  name,
+  commentsCount }:ITopicWidgetProps): JSX.Element => {
+  const [mouseEnter, setMouseEnter] = useState<boolean>(false);
+
+  // При наведении на блок даты возвращаю нужный класс
+  const dateClass = mouseEnter ? classes.date__visible : classes.date__hidden;
+
+  // Получаю вьюпорт и отрисовываю дивайдер при ширине > 800
+  const divider = window.innerWidth > 800 ? classes.divider : null;
+
+  return (
+    <article className={classes.article}>
+      <div className={classes.article__contentContainer}>
+        <Link to="#" className={classes.article__link}>{title}</Link>
+        <p className={classes.article__content}>{content}</p>
+      </div>
+      <div className={`${classes.article__userInfoContainer} ${divider}`}>
+        <img className={classes.article__userAvatar} src='#' alt='' />
+        <span className={classes.article__userName}>{name}</span>
+      </div>
+      <div
+        className={`${classes.article__dateContainer} ${divider}`}
+        onMouseEnter={() => setMouseEnter(() => true)}
+        onMouseLeave={() => setMouseEnter(() => false)}
+      >
+        <div className={classes.date}>
+          <img className={classes.date__icon} src={calendarIcon} alt='Calendar icon' />
+          <span className={classes.date__info}>
+            {format(creationDate, 'd LLLL, HH:mm', { locale: ru })}
+          </span>
+        </div>
+        <div className={dateClass}>
+          <div className={classes.date}>
+            <img className={classes.date__icon} src={calendarIcon} alt='Calendar icon' />
+            <span className={classes.date__info}>
+              {format(creationDate, 'dd.MM.yyyy HH:mm')}
+            </span>
+          </div>
+          <div className={classes.date}>
+            <img className={classes.date__icon} src={calendarIcon} alt='Calendar icon' />
+            <span className={classes.date__info}>
+              {format(lastUpdateDate, 'dd.MM.yyyy HH:mm')}
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className={`${classes.article__commentsContainer} ${divider}`}>
+        <img src={commentsIcon} alt='Comments icon' />
+        <span>{commentsCount}</span>
+      </div>
+    </article>
+  );
+};
+
+export default TopicWidget;
