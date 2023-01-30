45ba4a7836dd79c77eea23fc1dcf1a88d80cbe08
Stanislav Dokshin <d08121988sv@mail.ru>
2023-01-30 18:50:18 +0300

    create the basis

---
+++ src/widgets/CalendarWidget/СalendarWidget.tsx
@@ -0,0 +1,11 @@
+import classes from './СalendarWidget.module.scss';
+
+function СalendarWidget(): JSX.Element {
+  return (
+    <div className={classes.calendarWidget_container}>
+      <div />
+    </div>
+  );
+}
+
+export default СalendarWidget;
