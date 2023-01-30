f76a473b99455994f6bd5c98762589d0b3699e82
Andrey Khyshev <andrey6lock@gmail.com>
2023-01-30 17:22:44 +0300

    SP-18: Add ForumPage wrapper

--- src/view/User/ForumPage/ForumPage.tsx
+++ src/view/User/ForumPage/ForumPage.tsx
@@ -1,5 +1,20 @@
+import classes from './ForumPage.module.scss';
+
 function ForumPage() {
-  return <div>Forum Page</div>;
+  return (
+    <section className={classes.wrapper}>
+      <div className={classes.contentContainer}>
+        <header className={classes.headerContainer}>
+          <h1 className={classes.pageTitle}>
+            Форум
+          </h1>
+        </header>
+        <main className={classes.content}>
+          <ul className={classes.content__list} />
+        </main>
+      </div>
+    </section>
+  );
 }
 
 export default ForumPage;
