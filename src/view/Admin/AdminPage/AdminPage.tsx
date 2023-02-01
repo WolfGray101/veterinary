6b207c1f69ebf14308aa67080a5cacee3f8caf6d
Andrey Khyshev <andrey6lock@gmail.com>
2023-02-01 12:02:46 +0300

    SP-20: Create AdminPannel, fix Header and Input

--- src/view/Admin/AdminPage/AdminPage.tsx
+++ src/view/Admin/AdminPage/AdminPage.tsx
@@ -1,5 +1,29 @@
+import classes from './AdminPage.module.scss';
+
+import Input from '../../../shared/Input/Input';
+import useInput from '../../../hooks/useInput';
+
 function AdminPage() {
-  return <div>Admin Page</div>;
+  const searchInput = useInput('');
+
+  return (
+    <div className={classes.adminPageContainer}>
+      <aside>
+        <div />
+      </aside>
+      <div className={classes.adminMain}>
+        <div className={classes.adminMain__search}>
+          <Input icon {...searchInput} />
+        </div>
+        <header className={classes.adminMain__header}>
+          <h1 className={classes.adminMain__title}>
+            Admin
+          </h1>
+        </header>
+        <section />
+      </div>
+    </div>
+  );
 }
 
 export default AdminPage;
