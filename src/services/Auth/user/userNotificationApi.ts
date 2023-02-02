aa3badabdcbd43ab17dfc0431dac31e3c07917e3
tastyk0yla <107864983+tastyk0yla@users.noreply.github.com>
2023-02-02 17:49:52 +0300

    24: Create userNotificationApi

---
+++ src/services/Auth/user/userNotificationApi.ts
@@ -0,0 +1,37 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { RootState } from '../../../store/store';
+
+type Id = number;
+
+export const userNotificationApi = createApi({
+  reducerPath: 'commentApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://91.241.64.154:8080/api/user/notification/',
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).user.jwtToken;
+
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+
+      return headers;
+    },
+  }),
+  endpoints: (build) => ({
+    getAllNotifications: build.query({
+      query: () => '',
+    }),
+    getNotification: build.query({
+      query: (id: Id) => `${id}`,
+    }),
+    unnamedRequest: build.mutation({
+      query: (id: Id) => ({
+        url: `${id}`,
+        method: 'PUT',
+      }),
+    }),
+  }),
+});
+
+export const { useGetAllNotificationsQuery, useGetNotificationQuery, useUnnamedRequestMutation } =
+  userNotificationApi;
