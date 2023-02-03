78f35c6f53faae7cfbd3898ab084cf3f9cb89c52
Stanislav Dokshin <d08121988sv@mail.ru>
2023-02-03 14:44:23 +0300

    correction of remarks from the team lead

--- src/widgets/cardWidget/CardWidget.tsx
+++ src/widgets/cardWidget/CardWidget.tsx
@@ -2,15 +2,15 @@ import { ReactNode } from 'react';
 import classes from '../../styles/cardWidget.module.scss';
 
 type PropsType = {
-  children: ReactNode;
+  props: ReactNode;
 };
 
-function CardWidget({ children }: PropsType): JSX.Element {
+function CardWidget({ props }: PropsType): JSX.Element {
   return (
     <div className={classes.card}>
       <div className={classes.cardContent}>
         <div className={classes.cardDecorativeLine} />
-        {children}
+        {props}
       </div>
     </div>
   );
